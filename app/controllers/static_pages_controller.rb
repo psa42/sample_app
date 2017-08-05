7d3297419521c5c26711fb36de0a06d55422e610
patrice <psalnot@lesinfluenceurs.fr>
2017-08-05 05:55:36 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
 class StaticPagesController < ApplicationController
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   def home
   end
 
   def help
   end
+  
+  def about
+  end
+  
 end

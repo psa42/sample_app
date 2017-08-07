5828227a5a234a728be8b34bb13c8a6c42d640ab
patrice <psalnot@lesinfluenceurs.fr>
2017-08-07 11:36:48 +0000

    Finis user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   root 'static_pages#home'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
